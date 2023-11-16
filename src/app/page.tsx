2d4d98ae30a7a8a83ad8ff135d39372ad4c12a3a
Tim Restieaux <restieauxbro@hotmail.com>
2023-11-17 09:32:33 +1100

    iterator key

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,15 +11,15 @@ export default function Home() {
   return (
     <main>
       <div className="grid min-h-screen place-items-center">
-        <div className="w-full max-w-screen-lg grid">
+        <div className="grid w-full max-w-screen-lg">
           <div className="mb-24 max-w-3xl">
             <h1 className={cn(headingStyles, "mb-8 lg:text-6xl")}>
               <Balancer>A heading about bringing AI to industry</Balancer>
             </h1>
             <p className="max-w-lg text-lg lg:text-xl">
-              We development of smart digital solutions for businesses looking
-              forward. We’re a lean team who has cut the crap and trimmed the
-              fat of agency work yada yada yada AI
+              We develop smart digital solutions for businesses looking forward.
+              We’re a lean team who has cut the crap and trimmed the fat of
+              agency work yada yada yada AI
             </p>
           </div>
           <div className="grid gap-6">
@@ -40,8 +40,11 @@ export default function Home() {
                   title: "Suggestion 1",
                   description: "This is a description of suggestion 1",
                 },
-              ].map((suggestion) => (
-                <div className="rounded-lg border bg-zinc-100 p-4 shadow-md">
+              ].map((suggestion, i) => (
+                <div
+                  key={i}
+                  className="rounded-lg border bg-zinc-100 p-4 shadow-md"
+                >
                   <h2 className="font-medium">{suggestion.title}</h2>
                   <p>{suggestion.description}</p>
                 </div>
@@ -57,7 +60,7 @@ export default function Home() {
               <form onSubmit={handleSubmit}>
                 <Input
                   value={input}
-                  className="w-full text-lg py-6 px-5"
+                  className="w-full px-5 py-6 text-lg"
                   placeholder="Say something..."
                   onChange={handleInputChange}
                 />
